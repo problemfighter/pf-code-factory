b883663f5d93619b98ac3acb28f3b61bc6c68ec6
hmtmcse <hmtm.cse@gmail.com>
2020-12-29 12:05:18 +0600

    Integrated UI

---
+++ src/main/java/com/problemfighter/pfcodefactory/CodeFactoryScanner.java
@@ -0,0 +1,8 @@
+package com.problemfighter.pfcodefactory;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.problemfighter.pfcodefactory.*"})
+public class CodeFactoryScanner {
+}
